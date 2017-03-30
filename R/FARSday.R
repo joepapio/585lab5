a65a1d9976dacb1ad7900bd0b169f44740b028af
joepapio <joepapio@iastate.edu>
2017-03-30 16:45:28 -0500

    Update FARSday.R

--- R/FARSday.R
+++ R/FARSday.R
@@ -1,4 +1,11 @@
-#'require(tidyverse)
+#'library(tidyverse)
+#' Filter a dataset by day of the week.
+#' 
+#' @param data A dataset.
+#' @param day An integer value between 1 and 7, indicating day of the week
+#' @export
+#' @return A plot number of accidentws by hour for a given \code{day} of the week, split by whether or not one or more drunk driver was involved. s
+#' FARSday(accData, 3)
 
 FARSday <- function( data, day){
   data %>% filter(DAY_WEEK == day) %>%
