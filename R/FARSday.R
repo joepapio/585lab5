e57b1737189ee0915abf50246c4e50ca247fc8e3
emilyISU <chaohui.yuan@gmail.com>
2017-03-30 16:45:39 -0500

    add description

--- R/FARSday.R
+++ R/FARSday.R
@@ -1,16 +1,16 @@
-#'library(tidyverse)
-#' Filter a dataset by day of the week.
+#' Plot the number of accidents against hours for drunk driver and not drunk driver group separately 
 #' 
-#' @param data A dataset.
-#' @param day An integer value between 1 and 7, indicating day of the week
+#' @param data accData data set .
+#' @param day A number from 1 to 7.
 #' @export
-#' @return A plot number of accidentws by hour for a given \code{day} of the week, split by whether or not one or more drunk driver was involved. s
-#' FARSday(accData, 3)
+#' @return A figure of number of accidents against hours in day \code{y}.
+#' @examples
+#' FARSday(accData, 2)
+#' FARSday(accData, 7)
 
 FARSday <- function( data, day){
-  data %>% filter(DAY_WEEK == day) %>%
-    ggplot(aes(x=HOUR, y=n, color=dd)) +
+  data %>% dplyr::filter(DAY_WEEK == day) %>%
+    ggplot2::ggplot(aes(x=HOUR, y=n, color=dd)) +
     geom_point() +
     scale_color_manual(values=c("red", "blue"))
 }
-
