4872afc6c3252db95eeb6296606b988d2e78c18e
emilyISU <chaohui.yuan@gmail.com>
2017-03-30 16:33:17 -0500

    description for data set

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Summary of the number of accidents in fars database 
+#'
+#' A data set contains number of accidents, hours of accidents occuring and the if the drivers is drunk or not 
+#' 
+#'
+#' @format A data set contains 336 rows and 4  variables in this data set, Summary of the number of accidents and the time of the accidents occuring and whether the driver is drunk or not.   
+#' \describe{
+#'   \item{HOUR}{Hours of accidents occurring}
+#'   \item{DAY_WEEK}{Day of week, from Monday to Sunday, 1 - 7}
+#'   \item{dd}{Driver is drunk or not} 
+#'   \item{n}{Numer of accidents} 
+#'   ...
+#' }
+#' @source \url{https://www.nhtsa.gov/research-data/fatality-analysis-reporting-system-fars}
+"accData"
